f25182f48fe041d88e140c385014a7956360b7e5
A K <53090712+spacatty@users.noreply.github.com>
2026-01-22 03:42:28 +0300

    Fix resizable: use correct exports (Group, Panel, Separator)

--- frontend/src/components/ui/resizable.tsx
+++ frontend/src/components/ui/resizable.tsx
@@ -2,16 +2,16 @@
 
 import * as React from "react"
 import { GripVerticalIcon } from "lucide-react"
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
+import { Group, Panel, Separator } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 function ResizablePanelGroup({
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup>) {
+}: React.ComponentProps<typeof Group>) {
   return (
-    <PanelGroup
+    <Group
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
         className
@@ -31,11 +31,11 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & {
+}: React.ComponentProps<typeof Separator> & {
   withHandle?: boolean
 }) {
   return (
-    <PanelResizeHandle
+    <Separator
       className={cn(
         "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
         className
@@ -47,7 +47,7 @@ function ResizableHandle({
           <GripVerticalIcon className="h-2.5 w-2.5" />
         </div>
       )}
-    </PanelResizeHandle>
+    </Separator>
   )
 }
 
